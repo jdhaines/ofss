62df1bce06fe1d4c81718e5a93c77dade7a65988
Josh Haines <Josh@JoshHaines.com>
2016-07-08 14:22:29 -0400

    added flask templates

--- app/views.py
+++ app/views.py
@@ -1,10 +1,21 @@
 """Views module for the flask app."""
 
-from app import app
+# import csv
+
+# from io import StringIO
+
+from app import app  # , db, models
+
+from flask import render_template  # Markup, abort, make_response, , request
+
+# from .forms import BushingInfo, BushingSN, SingleExtract
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index", methods=['GET'])
 def index():
     """Display the home (index) page."""
-    return "Hello, World!"
+    return render_template("index.html",
+                           title="Ohio Family Survival Store",)
+
+# end
