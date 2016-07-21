69d90d202f2a8db1ab8817174e2db4eb16fcde76
Josh Haines <Josh@JoshHaines.com>
2016-07-21 13:52:49 -0400

    contact form showing on page

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 """Module which describes the form fields in the bushing app."""
 
 from flask_wtf import Form, RecaptchaField
-from wtforms import StringField  # , StringField, SelectField, TextAreaField
+from wtforms import StringField, TextAreaField  # , StringField, SelectField
 from wtforms import validators, SubmitField  # ValidationError,
 # from wtforms.validators import DataRequired
 
@@ -14,6 +14,8 @@ class ContactForm(Form):
     email = StringField("Email", [validators.Required(
         "Please enter your email address."), validators.Email(
         "Please enter your email address.")])
+    message = TextAreaField("Message", [validators.Required(
+        "Please enter a message.")])
     recaptcha = RecaptchaField()
     submit = SubmitField("Send")
 
